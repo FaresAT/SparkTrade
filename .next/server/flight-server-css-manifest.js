63d3ae3c73cac8704c009c6ddc695ff5bd573013
Fares Al-Tantawi <tantawi2001@gmail.com>
2023-12-26 18:16:50 -0500

    initial template

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/fares/repos/SparkTrade/app/layout.tsx": [
+      "/home/fares/repos/SparkTrade/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/home/fares/repos/SparkTrade/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/fares/repos/SparkTrade/app/page": [
+      "/home/fares/repos/SparkTrade/app/globals.css",
+      "/home/fares/repos/SparkTrade/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
